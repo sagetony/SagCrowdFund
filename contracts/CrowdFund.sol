19076f6d66ec77e3ed097503dc3d7531eae5ffa9
sagetony <uchetony224@gmail.com>
2023-04-19 16:12:27 +0100

    created a crowdFund

--- contracts/CrowdFund.sol
+++ contracts/CrowdFund.sol
@@ -2,4 +2,28 @@
 
 pragma solidity ^0.8.0;
 
-import 
+import "./SageToken.sol";
+
+contract CrowdFund {
+    // launch a campaign (creator, amountDonated, id, status, startAt, endAt)
+    struct Campaign {
+        address creator;
+        uint donation;
+        uint id;
+        uint goal;
+        uint32 startAt;
+        uint32 endAt;
+        bool claimed;
+    }    
+    Campaign[] public campaigns;
+    
+    function launch(uint _goal, uint _startAt, uint _startEnd) external {
+
+    }
+
+    function cancel(uint _id) external{}
+    function pledge(uint _id) external{}
+    function unpledge(uint _id) external{}
+    function claim(uint _id) external{}
+    function refund(uint _id) external{}
+}
